af52e2482c3d6d8329276477f4da286377babf80
alphaQ054002 <nkhan054002@gmail.com>
2023-01-28 23:11:37 +0530

    Push recent changes for pair programming

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,5 +1,8 @@
 import { OnRpcRequestHandler } from '@metamask/snap-types';
-
+import {nftAddress, nftAbi} from "./constants";
+import { Contract, providers } from 'ethers';
+import detectEthereumProvider from '@metamask/detect-provider';
+import {ethers} from "ethers"
 var adsMap = new Map<string, Object>([
   ['0x1', {
     heading: 'Its an add 1',
@@ -11,6 +14,29 @@ var adsMap = new Map<string, Object>([
     heading: 'Its an add 1',
   }],
 ]);
+
+// const MUMBAI_PROVIDER = new JsonRpcProvider("https://quiet-damp-brook.matic-testnet.discover.quiknode.pro/c73a64e337a259e2a9e69e3aec35a64c753bc426/");
+// const nftContract = new Contract(nftAddress, nftAbi, MUMBAI_PROVIDER);
+
+
+/**
+ * import { JsonRpcProvider, Contract } from 'ethers';
+import { ethers } from 'ethers';
+
+const MUMBAI_PROVIDER = new JsonRpcProvider("https://quiet-damp-brook.matic-testnet.discover.quiknode.pro/c73a64e337a259e2a9e69e3aec35a64c753bc426/");
+const nftContract = new Contract(nftAddress, nftAbi, MUMBAI_PROVIDER);
+
+async function getNFTData(): Promise<any> {
+  const ntemp = await nftContract.count();
+  const n = parseInt((ntemp).toString());
+  const idx = (Math.floor(Math.random()*n)).toString();
+  const ipfsUrl = await nftContract.tokenURI(idx);
+  const response = await fetch(ipfsUrl);
+  const json = await response.json();
+  return json.attributes;
+}
+
+ */
 
 /**
  * Get a message from the origin. For demonstration purposes only.
@@ -24,10 +50,32 @@ export const getMessage = (originString: string): string =>
 /**
  * Set random ad for each snap call
  */
-export function getRandomAd() {
-  let adsLen = adsMap.size;
-  const resKey = Array.from(adsMap.keys())[Math.floor(Math.random() * adsLen)];
-  return adsMap.get(resKey) as {heading: string};
+export async function getRandomAd() {
+  const provider = await detectEthereumProvider();
+  const web3Provider = new providers.Web3Provider(provider);
+  const nftContract = new Contract(nftAddress, nftAbi, web3Provider);
+  const ntemp = await nftContract.count();
+  const n = parseInt((ntemp).toString());
+  const idx = (Math.floor(Math.random()*n)).toString();
+  const ipfsUrl = await nftContract.tokenURI(idx);
+  const response = await fetch(ipfsUrl);
+  const json = await response.json();
+  return "asukdasudb"
+  // const MUMBAI_PROVIDER=new ethers.providers.JsonRpcProvider("https://quiet-damp-brook.matic-testnet.discover.quiknode.pro/c73a64e337a259e2a9e69e3aec35a64c753bc426/");
+  // const nftContract  = new Contract(nftAddress, nftAbi, MUMBAI_PROVIDER);
+  // const ntemp = await nftContract.count();
+  // // console.log(ntemp)
+  // const n = parseInt((ntemp).toString());
+  // const idx = (Math.floor(Math.random()*n)).toString();
+  // const ipfsUrl = await nftContract.tokenURI(idx);
+  // const response = await fetch(ipfsUrl);
+  // const json = await response.json();
+  // const attr = json.attributes;
+  // return "kasbjkdjabs";
+  ///AMRESH wrote the below thing
+  // let adsLen = adsMap.size;
+  // const resKey = Array.from(adsMap.keys())[Math.floor(Math.random() * adsLen)];
+  // return adsMap.get(resKey) as {heading: string};
 }
 
 /**
@@ -52,7 +100,7 @@ export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
             description:
               'Heading',
             textAreaContent:
-              `Its an main add ${getRandomAd().heading}`,
+              `Its an main add ${getRandomAd()}`,
           },
         ],
       });
