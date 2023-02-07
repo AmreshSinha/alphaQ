42b171d66af47316711344e2990ea74943b9f3fc
AmreshSinha <35039730+AmreshSinha@users.noreply.github.com>
2023-02-07 19:53:38 +0530

    Add contentId to response

--- packages/ad-portal/pages/api/fetchMetrics.ts
+++ packages/ad-portal/pages/api/fetchMetrics.ts
@@ -13,6 +13,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             }
         })
         res.status(200).json({metrics: {
+            contentId: contentId,
             views: count?.count,
         } , message: 'Success', error: false})
     } else {
