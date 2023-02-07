2d54bee7fcb753197cb519bb27010d096b6a7463
AmreshSinha <35039730+AmreshSinha@users.noreply.github.com>
2023-02-07 21:40:10 +0530

    Update metrics

--- packages/ad-portal/comps/metrics.tsx
+++ packages/ad-portal/comps/metrics.tsx
@@ -2,11 +2,19 @@ import { useState } from 'react';
 import styles from '@/styles/metrics.module.scss';
 import EyeSVG from '@/assets/Eye.svg';
 
-export default function Metrics({ count }: {count: number}) {
+export default function Metrics({ count }: {count?: number}) {
     const [metrics, setMetrics] = useState();
+    console.log(count)
     return (
         <div className={styles.metric_wrapper}>
-            {metrics==undefined ? (<div className={styles.clickOnAd}><h1>Click on Ad to see the metrics</h1></div>) : (<h1>Metrics</h1>)}
+            {count !== undefined ? <div className={styles.ad_views_wrapper}>
+                <img src={EyeSVG.src} />
+                <div className={styles.ad_views}>
+                    <h1>{count}</h1>
+                    <p>No of people who viewed the ad</p>
+                </div>
+            </div> : null}
+            {count===undefined ? (<div className={styles.clickOnAd}><h1>Click on Ad to see the metrics</h1></div>) : null}
         </div>
     )
 }
